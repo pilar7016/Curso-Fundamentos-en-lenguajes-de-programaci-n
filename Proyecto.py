8cb64248f6605c7ca059ddcfdcc2a575cd34f93c
your_github_username <your_github_email>
2023-03-08 12:10:32 -0500

    Proyecto Unidad 1

--- Proyecto.py
+++ Proyecto.py
@@ -6,12 +6,40 @@
 #Capturando la información desde el teclado
 id = input("Digite su documento de identidad: ") #método
 nombre = input("Nombre del cliente: ").title()
-tipo_cuenta = input("Tipo de cuenta: ").upper().strip()
-consignacion = float(input("Valor a consignar: "))
+tipo_cuenta = input("Tipo de cuenta: ").upper()
+if tipo_cuenta == "CORRIENTE" :
+    print("¡Tipo de cuenta sí existe - Continuar con consignación!")
+    consignacion = float(input("Valor a consignar: "))
+    if consignacion <= 30000:
+        print("¡No es posible realizar la consignación - Tope mínimo 3.000!")
+    elif consignacion >= 3000000:
+        print("¡No es posible realizar la consignación - Tope máximo 3.000.000!")
+    else:
+        print("¡Consignación realizada con éxito!")
+        #Mostrando información por pantalla
+        print("Valor consignado        : {:,.2f}".format(consignacion))
+        print("Identificación          : "+id)
+        print("Nombre                  : {}".format(nombre))
+        print(f"Tipo de cuenta         : {tipo_cuenta}")     
+elif tipo_cuenta == "AHORROS" :
+    print("¡Tipo de cuenta sí existe - Continuar con consignación!")
+    consignacion = float(input("Valor a consignar: "))
+    if consignacion <= 30000:
+        print("¡No es posible realizar la consignación - Tope mínimo 3.000!")
+    elif consignacion >= 3000000:
+        print("¡No es posible realizar la consignación - Tope máximo 3.000.000!")
+    else:
+        print("¡Consignación realizada con éxito!")  
+        #Mostrando información por pantalla
+        print("Valor consignado       : {:,.2f}".format(consignacion))
+        print("Identificación          : "+id)
+        print("Nombre                  : {}".format(nombre))
+        print(f"Tipo de cuenta         : {tipo_cuenta}")       
+else:
+    print("¡Tipo de cuenta no existe - Consignación cancelada!")
+    
+        
 
-#Mostrando información por pantalla
-print("Identificación          : "+id)
-print("Nombre                  : {}".format(nombre))
-print(f"Tipo de cuenta         : {tipo_cuenta}")
-print("Valor a consignar       : {:,.2f}".format(consignacion))
+        
+
 
